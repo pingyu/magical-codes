bf9d889d25e51461464ba8e19d58e8e6e0dff729
pingyu <shui.yu@126.com>
2021-10-13 21:32:40 +0800

    project 1 finish

--- rust2/projects/project-1/src/bin/kvs.rs
+++ rust2/projects/project-1/src/bin/kvs.rs
@@ -1,3 +1,36 @@
+use clap::{crate_version, load_yaml, App};
+use kvs::KvStore;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let app = App::from(yaml).version(crate_version!());
+    let m = app.get_matches();
+
+    let mut _kvs = KvStore::new();
+
+    match m.subcommand() {
+        ("get", Some(_sub_m)) => {
+            // let key = sub_m.index_of("KEY").unwrap();
+            // kvs.get(key.to_string());
+            // process::exit(0);
+            panic!("unimplemented");
+        }
+        ("set", Some(_sub_m)) => {
+            // let key = sub_m.index_of("KEY").unwrap();
+            // let val = sub_m.index_of("VALUE").unwrap();
+            // kvs.set(key.to_string(), val.to_string());
+            // process::exit(0);
+            panic!("unimplemented");
+        }
+        ("rm", Some(_sub_m)) => {
+            // let key = sub_m.index_of("KEY").unwrap();
+            // kvs.remove(key.to_string());
+            // process::exit(0);
+            panic!("unimplemented");
+        }
+        _ => {}
+    }
+
+    process::exit(1);
 }
--- rust2/projects/project-1/src/lib.rs
+++ rust2/projects/project-1/src/lib.rs
@@ -1,3 +1,42 @@
+// #![deny(missing_docs)]
+
+use std::collections::HashMap;
+
+/// KvStore is store of key-value.
+
+pub struct KvStore {
+    m: HashMap<String, String>,
+}
+
+impl KvStore {
+    /// Create a new KvStore object.
+    pub fn new() -> Self {
+        KvStore { m: HashMap::new() }
+    }
+
+    /// Set key to value. Overwrite if existed.
+    pub fn set(&mut self, key: String, value: String) {
+        self.m.insert(key, value);
+    }
+
+    /// Get value of key.
+    pub fn get(&self, key: String) -> Option<String> {
+        self.m.get(&key).map(|k| k.to_owned())
+    }
+
+    /// Remove entry of key.
+    /// Keep silence even if key is not existed.
+    pub fn remove(&mut self, key: String) {
+        self.m.remove(&key);
+    }
+}
+
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
